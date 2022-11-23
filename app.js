ec503b0eef1a33eaebe0100f76296b83bf5f25da
Santiago Gorosito <santigrst2@gmail.com>
2022-11-23 09:39:59 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,36 @@
+/*let nombre = "Santiago";
+let edad = 20;
+let alumno = true;
+
+console.log(nombre);
+console.log(edad);
+console.log(alumno);
+
+const CURSO = "Js";    
+
+console.log(CURSO);
+
+let numero1 = Number(prompt("Ingrese un numero para multiplicar"));
+let numero2 = Number(prompt("Ingrese el otro numero"));
+
+let resultado = numero1 * numero2;
+
+alert("El resultado de su multiplicación es =" + " " +resultado); */
+
+let pais = prompt("Ingrese de que pais eres ⬇️ ⬇️ ⬇️");
+let provincia = prompt("Ingrese de que provincia eres ⬇️ ⬇️ ⬇️");
+
+if (pais != "" && provincia != "") {
+    alert("Usted vive en: " + pais + " 😁" + "   En la provincia de: " + provincia + " 😁");
+} else {
+    alert("Error al ingresar sus datos");
+}
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
